2930ccbb13007a723345c1722e310f0ddb991467
vesper <kamilsoloducha@gmail.com>
2017-09-12 06:59:07 +0200

    Add TeachController, Teach View

---
+++ Oazachaosu/Oazachaosu/Controllers/TeachController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oazachaosu.Controllers
+{
+    public class TeachController : Controller
+    {
+
+        // GET: Teach
+        [Route("teach/{groupId}", Name = "TeachIndex")]
+        public ActionResult Index(int? groupId)
+        {
+            return View();
+        }
+    }
+}
