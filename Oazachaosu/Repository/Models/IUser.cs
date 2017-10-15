b9c66d0318d9f88748d144049361f3482782145c
vesper <kamilsoloducha@gmail.com>
2017-10-15 20:30:47 +0200

    impleements IUser in user object (make new fields in iuser interface to fit interface to class)

--- Oazachaosu/Repository/Models/IUser.cs
+++ Oazachaosu/Repository/Models/IUser.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using Repository.Models.Enums;
+using System;
 
-namespace Repository.Models {
-  public interface IUser {
-    long LocalId { get; set; }
-    string Name { get; set; }
-    string Password { get; set; }
-    string ApiKey { get; set; }
-    DateTime CreateDateTime { get; set; }
-    DateTime LastLoginDateTime { get; set; }
-    bool IsAdmin { get; set; }
+namespace Repository.Models
+{
+    public interface IUser
+    {
+        long LocalId { get; set; }
+        string Name { get; set; }
+        string Password { get; set; }
+        string ApiKey { get; set; }
+        DateTime CreateDateTime { get; set; }
+        DateTime LastLoginDateTime { get; set; }
+        bool IsAdmin { get; set; }
 
-  }
+        bool IsLogin { get; set; }
+        bool IsRegister { get; set; }
+        DateTime DownloadTime { get; set; }
+        TranslationDirection TranslationDirection { get; set; }
+        bool AllWords { get; set; }
+        int Timeout { get; set; }
+    }
 }
