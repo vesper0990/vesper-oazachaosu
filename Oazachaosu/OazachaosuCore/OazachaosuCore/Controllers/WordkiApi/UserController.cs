93ec6b20a11d3ce9009bf4b62afa80c671bb68cc
DS\KFG864 <kamil.soloducha@motorolasolutions.com>
2018-01-18 12:40:32 +0100

    Add generate apiKey

--- Oazachaosu/OazachaosuCore/OazachaosuCore/Controllers/WordkiApi/UserController.cs
+++ Oazachaosu/OazachaosuCore/OazachaosuCore/Controllers/WordkiApi/UserController.cs
@@ -3,6 +3,7 @@ using OazachaosuCore.Helpers;
 using OazachaosuCore.Helpers.Respone;
 using Repository;
 using Repository.Model.DTOConverters;
+using System;
 using System.Linq;
 using System.Net;
 using WordkiModelCore.DTO;
@@ -43,6 +44,7 @@ namespace OazachaosuCore.Controllers
                 return StatusCode((int)HttpStatusCode.Found);
             }
             User user = UserConverter.GetModelFromDTO(userDto);
+            user.ApiKey = RandomString(32);
             Repository.AddUser(user);
             Repository.SaveChanges();
             userDto = UserConverter.GetDTOFromModel(Repository.GetUsers().SingleOrDefault(x => x.Name.Equals(userDto.Name)));
@@ -52,13 +54,12 @@ namespace OazachaosuCore.Controllers
         [HttpPut]
         public IActionResult Put([FromBody] UserDTO userDto)
         {
-            User user = Repository.GetUsers().SingleOrDefault(x => x.Name.Equals(userDto.Name) && x.Password.Equals(userDto.Password));
+            User user = Repository.GetUsers().SingleOrDefault(x => x.ApiKey.Equals(userDto.ApiKey));
             if (user == null)
             {
                 return StatusCode((int)HttpStatusCode.NotFound);
             }
             user.Password = userDto.Password;
-            user.ApiKey = userDto.Password;
             Repository.UpdateUser(user);
             Repository.SaveChanges();
             user = Repository.GetUsers().SingleOrDefault(x => x.Name.Equals(userDto.Name));
@@ -112,28 +113,36 @@ namespace OazachaosuCore.Controllers
             return new JsonResult(result);
         }
 
-        [HttpPut]
-        public IActionResult Update2([FromServices] IHeaderElementProvider headerElementProvider)
+        private static Random random = new Random();
+        public static string RandomString(int length)
         {
-            ApiResult result = new ApiResult();
-            string userName = headerElementProvider.GetElement(Request, "userName");
-            string hashPassword = headerElementProvider.GetElement(Request, "password");
-            string newHashPassword = headerElementProvider.GetElement(Request, "newPassword");
-            User user = Repository.GetUsers().SingleOrDefault(x => x.Name.Equals(userName) && x.Password.Equals(hashPassword));
-            if (user == null)
-            {
-                result.Code = ResultCode.UserNotFound;
-                return new JsonResult(result);
-            }
-
-            user.Password = newHashPassword;
-            user.ApiKey = newHashPassword;
-            Repository.UpdateUser(user);
-            Repository.SaveChanges();
-            user = Repository.GetUsers().SingleOrDefault(x => x.Name.Equals(userName));
-            result.Object = UserConverter.GetDTOFromModel(user);
-            result.Code = ResultCode.Done;
-            return new JsonResult(result);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
+
+        //[HttpPut("Put2")]
+        //public IActionResult Update2([FromServices] IHeaderElementProvider headerElementProvider)
+        //{
+        //    ApiResult result = new ApiResult();
+        //    string userName = headerElementProvider.GetElement(Request, "userName");
+        //    string hashPassword = headerElementProvider.GetElement(Request, "password");
+        //    string newHashPassword = headerElementProvider.GetElement(Request, "newPassword");
+        //    User user = Repository.GetUsers().SingleOrDefault(x => x.Name.Equals(userName) && x.Password.Equals(hashPassword));
+        //    if (user == null)
+        //    {
+        //        result.Code = ResultCode.UserNotFound;
+        //        return new JsonResult(result);
+        //    }
+
+        //    user.Password = newHashPassword;
+        //    user.ApiKey = newHashPassword;
+        //    Repository.UpdateUser(user);
+        //    Repository.SaveChanges();
+        //    user = Repository.GetUsers().SingleOrDefault(x => x.Name.Equals(userName));
+        //    result.Object = UserConverter.GetDTOFromModel(user);
+        //    result.Code = ResultCode.Done;
+        //    return new JsonResult(result);
+        //}
     }
 }
