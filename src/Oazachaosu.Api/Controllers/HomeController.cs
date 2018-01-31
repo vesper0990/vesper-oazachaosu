6ae72526492d6c3b11d26a60506e21417b1d5b66
DS\KFG864 <kamil.soloducha@motorolasolutions.com>
2018-01-31 13:41:17 +0100

    Add example file downloader

--- src/Oazachaosu.Api/Controllers/HomeController.cs
+++ src/Oazachaosu.Api/Controllers/HomeController.cs
@@ -1,14 +1,63 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Oazachaosu.Api.Models;
 
 namespace Oazachaosu.Api.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("File/{filename}")]
+        public async Task<IActionResult> File(string filename)
+        {
+            if (filename == null)
+                return Content("filename not present");
+
+            var path = Path.Combine(
+                           Directory.GetCurrentDirectory(),
+                          filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(path), Path.GetFileName(path));
+        }
+
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".zip", "application/zip"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"}
+            };
         }
 
         public IActionResult About()
